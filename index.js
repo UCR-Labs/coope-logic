7bb079e18da5e620c22c6bed20fe9e07ded611f9
Javier Donato <javidonatoh01@gmail.com>
2024-05-20 23:08:47 -0600

    Documentacion de la funcion echo

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ import * as functions from 'firebase-functions';
 
 //Incluir todas las clases que desean exportar
 export { SQLiteFunctions } from "./src/storage/SQLite";
+
+
+/**
+ * Función Echo que responde con el mismo mensaje recibido en la solicitud.
+ * @param {Object} request - El objeto de solicitud HTTP.
+ * @param {Object} response - El objeto de respuesta HTTP.
+ */
