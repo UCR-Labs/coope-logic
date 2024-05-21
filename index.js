0a6890fcfa14c78b158dc5185cd90d2d2e1525bf
Javier Donato <javidonatoh01@gmail.com>
2024-05-20 23:00:24 -0600

    Crear branch y agregar dependencia al index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+// Importar las dependencias necesarias
+import * as functions from 'firebase-functions';
+
 //Incluir todas las clases que desean exportar
 export { SQLiteFunctions } from "./src/storage/SQLite";
