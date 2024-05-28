c99eef0ce3fe8acd2824202b5639138a4035849d
Cristian Martínez <crismh2@hotmail.com>
2024-05-27 23:50:42 -0600

    Done

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 //Incluir todas las clases que desean exportar
 export { SQLiteFunctions } from "./src/storage/SQLite";
+export { CacheFunctions } from "./src/storage/Cache";
+export { LocalStorageFunctions } from "./src/storage/LocalStorage";
