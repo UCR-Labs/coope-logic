cf2b56d7eeb6a78264341c4dde5547d90ee092d0
Cristian Martínez <crismh2@hotmail.com>
2024-05-28 12:37:19 -0600

    Add Typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// index.d.ts
+declare module "coope-logic" {
+  // Replace `any` with the actual types of your functions
+  export function SQLiteFunctions(): any;
+  export function CacheFunctions(): any;
+  export function LocalStorageFunctions(): any;
+}
