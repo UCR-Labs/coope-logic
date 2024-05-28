7974f7c41309bc3e2d9aacb1cddd444dee327511
Cristian Martínez <crismh2@hotmail.com>
2024-05-28 16:00:42 -0600

    Done

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-// index.d.ts
-
-import { SQLiteFunctions } from "./src/storage/SQLite";
-export { SQLiteFunctions };
-import { CacheFunctions } from "./src/storage/Cache";
-export { CacheFunctions };
-import { LocalStorageFunctions } from "./src/storage/LocalStorage";
-export { LocalStorageFunctions };
