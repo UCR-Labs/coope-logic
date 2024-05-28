8cf0089a20a46adfcf5fcdfb7a0865c77eaf4f49
Cristian Martínez <crismh2@hotmail.com>
2024-05-28 16:42:04 -0600

    Done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
 //Incluir todas las clases que desean exportar
-const SQLiteFunctions = require("./src/storage/SQLite");
+//const SQLiteFunctions = require("./src/storage/SQLite");
 const CacheFunctions = require("./src/storage/Cache");
-const LocalStorageFunctions = require("./src/storage/LocalStorage");
+//const LocalStorageFunctions = require("./src/storage/LocalStorage");
 
 module.exports = {
-  SQLiteFunctions,
+  //SQLiteFunctions,
   CacheFunctions,
-  LocalStorageFunctions
+  //LocalStorageFunctions
 };
