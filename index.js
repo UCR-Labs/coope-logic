5d2395154e736f806b8704f8f9a827f6966dc4a0
Javier Donato <javidonatoh01@gmail.com>
2024-05-27 22:46:44 -0600

    Restaurar el index.js

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-// Importar las dependencias necesarias
-const functions = require('firebase-functions');
-
 //Incluir todas las clases que desean exportar
-//export { SQLiteFunctions } from "./src/storage/SQLite";
-
-
-/**
- * Función Echo que responde con el mismo mensaje recibido en la solicitud.
- * @param {Object} request - El objeto de solicitud HTTP.
- * @param {Object} response - El objeto de respuesta HTTP.
- */
-
-module.exports = {
-    SQLiteFunctions: require("./src/storage/SQLite").SQLiteFunctions,
-    echo: functions.https.onRequest((request, response) => {
-      const message = request.body.message;
-      response.send({ message });
-    })
-  };
+export { SQLiteFunctions } from "./src/storage/SQLite";
