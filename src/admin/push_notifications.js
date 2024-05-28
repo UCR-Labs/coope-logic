4c9e9cdf718815e26e7e149c404d817e1ae0f473
Jafet Picado <86694602+Jafet-PicadoQ@users.noreply.github.com>
2024-05-27 20:10:39 -0600

    Cambio en la lógica de manejo de notificaciones

--- src/admin/push_notifications.js
+++ src/admin/push_notifications.js
@@ -7,29 +7,23 @@ admin.initializeApp();
  * Obtiene un documento con la notificación a enviar, y realiza la lógica
  * necesaria para enviar dicha notificación a los dispositivos correspondientes
  */
-const adminPushNotifications = functions.firestore
-  .document('notifications/{notificationId}')
-  .onCreate(async (snapshot) => {
-    const title = snapshot.data().title;
-    const body = snapshot.data().body;
-    const tokens = snapshot.data().tokens;
-    const filteredTokens = tokens.filter((token) => token.length > 0);
-
-    const payload = {
-      notification: {
-        title: title,
-        body: body,
-      },
-    };
-    if (filteredTokens.length > 0) {
-      const messages = filteredTokens.map((token) => ({
-        token: token,
-        data: payload,
-      }));
-      
-      await admin.messaging().sendEach(messages);
-      console.log('Notifications have been sent and tokens cleaned up.');
-    }
-  });
+const adminPushNotifications = async (title, body, tokens) => {
+  const filteredTokens = tokens.filter((token) => token.length > 0);
+  const payload = {
+    notification: {
+      title: title,
+      body: body,
+    },
+  };
+  if(filteredTokens.length > 0) {
+    const messages = filteredTokens.map((token) => ({
+      token: token,
+      data: payload,
+    }));
+    
+    await admin.messaging().sendEach(messages);
+    console.log('Notifications have been sent and tokens cleaned up.');
+  }
+};
 
 module.exports = { adminPushNotifications };
